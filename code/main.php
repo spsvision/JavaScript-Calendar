1ba626e4cf5ff431ec7c6214293b7f2573895fcf
spsvision <steven-ps@hotmail.com>
2017-05-16 21:13:55 -0700

    main.php
    Standard home page with a link to open the calendar modal

---
+++ code/main.php
@@ -0,0 +1,26 @@
+<?php
+    require_once("cal.php");
+?>
+<a href="#" class="icon-info" data-toggle="modal" data-target="#calendar">open Calendar</a>
+
+<!-- Calendar #Calendar Modal-->
+<div id="calendar" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog">
+  <div class="modal-content">
+      <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+      </div>
+      <div class="modal-body">
+      	  <button id="btnPrev" type="button">Prev</button>
+	  <button id="btnNext" type="button">Next</button>
+  	  <div  id="divCal"></div>
+
+      </div>
+      <div class="modal-footer">
+          <div class="col-md-12">
+          <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
+      </div>
+      </div>
+  </div>
+  </div>
+</div>
